bc441cc8c6096bd95ce08a1009b12d8ded9f9b7e
UnknownRussian <37229726+UnknownRussian@users.noreply.github.com>
2022-12-22 14:05:08 +0100

    New stuff added

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        SomeRandomMethod srm = new SomeRandomMethod("I created this");
+    }
+}
---
+++ src/SomeRandomMethod.java
@@ -0,0 +1,5 @@
+public class SomeRandomMethod{
+    public SomeRandomMethod(String msg){
+        System.out.println("Your message is: "+msg);
+    }
+}
